dc30c6d42b663280217690080987336c9003cf4b
Matt Rosenthal <matt.rosenthal@gmail.com>
2014-10-16 23:02:29 -0700

    collecting ips

--- collect_ip.py
+++ collect_ip.py
@@ -2,58 +2,90 @@ import pcapy
 import sys
 import parse_pcapy_packet
 import socket
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import IntegrityError
+import datetime
+from sqlalchemy_db_setup import Base, User, IPAddress, VisitByUser
+
+
+
 
 def main(argv):
-	dev = argv[1]
+    dev = argv[1]
 
-	print "dev = {}".format(dev)
-	bpffilter = "ip dst {} and not src 192.168.0".format(argv[2])
-	print "bpf = {}".format(bpffilter)
+    print "dev = {}".format(dev)
+    bpffilter = "ip dst {} and not src 192.168.0".format(argv[2])
+    print "bpf = {}".format(bpffilter)
 
-	cap = pcapy.open_live(dev, 100,1,5000)
-	cap.setfilter(bpffilter)
-	while(1):
-		try:
-			(header,packet) = cap.next()
-#			print "packet = {}".format(packet)
-			if packet:
-				process_packet(packet)
-		except pcapy.PcapError:
-			#print "pcapy error"
-			pass
-#			exit(-1)
-		except StopIteration:
-			print "cap is empty"
-			exit(-1)
+    engine = create_engine('sqlite:///sqlalchemy_sqlite.db')
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    
+    new_user = User(fullName='Matt', vpnUserName='matt', vpnPassword='vpn_password33')
+    session.add(new_user)
+    session.commit()
 
-	#create capture interface
-	#loop to process packets
+    
 
-	
+    cap = pcapy.open_live(dev, 100,1,5000)
+    cap.setfilter(bpffilter)
+    while(1):
+        try:
+            (header,packet) = cap.next()
+#           print "packet = {}".format(packet)
+            if packet:
+                process_packet(packet, session, new_user)
+        except pcapy.PcapError:
+            #print "pcapy error"
+            pass
+#           exit(-1)
+        except StopIteration:
+            print "cap is empty"
+            exit(-1)
 
-def process_packet(packet):
-	#parse ip_src, ip_dst
-	#send to DB
-	try:
-		ip_src, ip_dst = parse_pcapy_packet.parse_packet(packet)
-		send_ip_db(ip_src, ip_dst)
-		#break
-	except TypeError:
-		print "not a valid ethernet packet"
+    #create capture interface
+    #loop to process packets
+
+    
+
+def process_packet(packet, session, user):
+    #parse ip_src, ip_dst
+    #send to DB
+    try:
+        ip_src, ip_dst = parse_pcapy_packet.parse_packet(packet)
+        send_ip_db(ip_src, session, user)
+        #break
+    except TypeError:
+        print "not a valid ethernet packet"
 
 count = 0
 
-def send_ip_db(ip_src, ip_dst):
-	global count
-	hostname = "unknown host"
-	try:
-		hostname = socket.gethostbyaddr(ip_src)
-	except socket.herror:
-		print "could not lookup {}".format(ip_src)
+def send_ip_db(ip_src, session, user):
+    global count
+    hostname = "unknown host"
+    try:
+        hostname = socket.gethostbyaddr(ip_src)
+    except socket.herror:
+        print "could not lookup {}".format(ip_src)
 
-	print "{} : packet from ip={} : hostname={}".format(count,str(ip_src), hostname[0])
-	count += 1
+    print "{} : packet from ip={} : hostname={}".format(count,str(ip_src), hostname[0])
+    subnet = ip_src.rsplit(".",1)[0]
+    new_ip = IPAddress(IP=ip_src, subnet=subnet)
+    try:
+        session.add(new_ip)
+        session.commit()
+    except IntegrityError:
+        session.rollback()
+        new_ip = session.query(IPAddress).filter(IPAddress.IP==ip_src)[0]
+    new_visit = VisitByUser(user=user,IP=new_ip, dateTime=datetime.datetime.now())
+    session.add(new_visit)
+    session.commit()
+
+    count += 1
 
 
 if __name__ == "__main__":
-	  main(sys.argv)
+      main(sys.argv)
--- db_test.py
+++ db_test.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, distinct 
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 import datetime
 
 from sqlalchemy_db_setup import Base, User, IPAddress, VisitByUser
-
+import socket
+import sys
 
 engine = create_engine('sqlite:///sqlalchemy_sqlite.db')
 
@@ -40,10 +41,46 @@ users = session.query(User).all()
 for user in users:
     print "User = {}".format(user.fullName)
 
+#exit(-1)
+
 IPs = session.query(IPAddress).all()
 for IP in IPs:
     print "IP = {} : subnet = {}".format(IP.IP, IP.subnet)
 
-visits = session.query(VisitByUser).all()
-for visit in visits:
-    print "visit: userid = {}, IPID = {}, datetime={}".format(visit.userID, visit.IPID, visit.dateTime)
+#visits = session.query(VisitByUser).all()
+#for visit in visits:
+#    print "visit: userid = {}, IPID = {}, datetime={}".format(visit.userID, visit.IPID, visit.dateTime)
+
+
+#print "len of IP = {}, len of visits = {}".format(len(IPs), len(visits))
+#unique_subnets = session.query(distinct(IPAddress.subnet)).all()
+
+
+subnets = session.query(distinct(IPAddress.subnet)).all()
+#subnets = session.query(IPAddress.subnet).all()
+def get_host(ip):
+
+    hostname = "No DNS Resolution"
+    if len(ip.split(".")) == 3:
+        ip = "{}.1".format(ip)
+    try:
+        hostname = socket.gethostbyaddr(ip)
+    except socket.herror:
+        print "could not resolve {}".format(ip)
+    return hostname
+
+ignore_hosts = []
+import fileinput
+for subnet in subnets:
+    subnet = subnet[0].encode("ascii")
+    hostname = get_host(subnet)
+    print "\nDo you want to block {} : {}".format(subnet, hostname)
+    answer = sys.stdin.readline().upper()
+    if "Y" in answer:
+        print "saving {} {} to DB".format(hostname, subnet)
+        ignore_hosts.append(subnet)
+
+
+
+for ip in ignore_hosts:
+    print "{}".format(ip)

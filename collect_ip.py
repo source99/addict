91a84503cf5f85a866f9d5f5c997cd754c7b5a4e
matt <matt.rosenthal@gmail.com>
2014-10-20 18:56:42 +0000

    updates to run on AWS

--- collect_ip.py
+++ collect_ip.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 import datetime
-from sqlalchemy_db_setup import Base, User, IPAddress, VisitByUser
+from sqlalchemy_db_setup import Base, User, IPAddress, VisitByUser, Subnet
 
 
 
@@ -19,25 +19,33 @@ def main(argv):
     bpffilter = "ip dst {} and not src 192.168.0".format(argv[2])
     print "bpf = {}".format(bpffilter)
 
-    engine = create_engine('sqlite:///sqlalchemy_sqlite.db')
+    engine = create_engine('mysql+mysqldb://vpn:ma93a-ya#A6@50.18.211.139:3306/vpn?charset=utf8&use_unicode=0')
     Base.metadata.bind = engine
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
     
-    new_user = User(fullName='Matt', vpnUserName='matt', vpnPassword='vpn_password33')
-    session.add(new_user)
-    session.commit()
+    user_exists = session.query(User).filter(User.vpnUserName=='matt').all()
+    if not user_exists:
+        curr_user = User(fullName='Matt', vpnUserName='matt', vpnPassword='vpn_password33')
+        session.add(curr_user)
+        session.commit()
+    else:
+        curr_user = session.query(User).filter(User.vpnUserName=='matt')[0]
 
     
 
     cap = pcapy.open_live(dev, 100,1,5000)
-    cap.setfilter(bpffilter)
+#    cap.setfilter(bpffilter)
+
+    cap.setfilter("not src 172.31.5.207 and not src 172.31.0.2 and not src 76.21.33.185 and not src 50.18.211.139 and not src 54.215.164.159 and not src 192.168.11.0 and not src 192.168.11.100 and not src 192.168.11.101")
     while(1):
         try:
             (header,packet) = cap.next()
+#            print "header= {}".format(str(header))
+#            print "packet = {}".format(packet)
 #           print "packet = {}".format(packet)
             if packet:
-                process_packet(packet, session, new_user)
+                process_packet(packet, session, curr_user)
         except pcapy.PcapError:
             #print "pcapy error"
             pass
@@ -45,6 +53,8 @@ def main(argv):
         except StopIteration:
             print "cap is empty"
             exit(-1)
+        except socket.timeout:
+            pass
 
     #create capture interface
     #loop to process packets
@@ -54,12 +64,15 @@ def main(argv):
 def process_packet(packet, session, user):
     #parse ip_src, ip_dst
     #send to DB
+    valid = True
     try:
         ip_src, ip_dst = parse_pcapy_packet.parse_packet(packet)
-        send_ip_db(ip_src, session, user)
         #break
     except TypeError:
         print "not a valid ethernet packet"
+        valid = False
+    if valid:
+        send_ip_db(ip_src, session, user)
 
 count = 0
 
@@ -69,18 +82,29 @@ def send_ip_db(ip_src, session, user):
     try:
         hostname = socket.gethostbyaddr(ip_src)
     except socket.herror:
-        print "could not lookup {}".format(ip_src)
+        hostname = "unknown"
 
-    print "{} : packet from ip={} : hostname={}".format(count,str(ip_src), hostname[0])
-    subnet = ip_src.rsplit(".",1)[0]
-    new_ip = IPAddress(IP=ip_src, subnet=subnet)
+#    print "{} : packet from ip={} : hostname={}".format(count,str(ip_src), hostname[0])
+    subnet = "{}.0/24".format(ip_src.rsplit(".",1)[0])
+    new_ip = IPAddress(IP=ip_src)
+    new_subnet = Subnet(subnet=subnet)
     try:
         session.add(new_ip)
         session.commit()
+        print "adding new ip"
     except IntegrityError:
+#        print "could not add new_ip because of integrity error - {}".format(new_ip.IP)
         session.rollback()
         new_ip = session.query(IPAddress).filter(IPAddress.IP==ip_src)[0]
-    new_visit = VisitByUser(user=user,IP=new_ip, dateTime=datetime.datetime.now())
+    try:
+        session.add(new_subnet)
+        session.commit()
+        print "adding new subnet"
+    except IntegrityError:
+        session.rollback()
+        new_subnet = session.query(Subnet).filter(Subnet.subnet==subnet)[0]
+
+    new_visit = VisitByUser(user=user,IP=new_ip, subnet = new_subnet, dateTime=datetime.datetime.now())
     session.add(new_visit)
     session.commit()
 
--- parse_pcapy_packet.py
+++ parse_pcapy_packet.py
@@ -5,41 +5,42 @@ import sys
 
 
 def eth_addr (a) :
-	b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
-	return b
+    b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (ord(a[0]) , ord(a[1]) , ord(a[2]), ord(a[3]), ord(a[4]) , ord(a[5]))
+    return b
 
 def parse_packet(packet) :
 
-	#parse ethernet header
-	eth_length = 14
+    #parse ethernet header
+    eth_length = 14
 
-	eth_header = packet[:eth_length]
-	eth = unpack('!6s6sH' , eth_header)
-	eth_protocol = socket.ntohs(eth[2])
-#	print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
+    eth_header = packet[:eth_length]
+    eth = unpack('!6s6sH' , eth_header)
+    eth_protocol = socket.ntohs(eth[2])
+    s_addr = "should not show"
+    d_addr = "should not show"
+#   print 'Destination MAC : ' + eth_addr(packet[0:6]) + ' Source MAC : ' + eth_addr(packet[6:12]) + ' Protocol : ' + str(eth_protocol)
 
-	#Parse IP packets, IP Protocol number = 8
-	if eth_protocol == 8 :
-		#Parse IP header
-		#take first 20 characters for the ip header
-		ip_header = packet[eth_length:20+eth_length]
+    #Parse IP packets, IP Protocol number = 8
+    if eth_protocol == 8 :
+        #Parse IP header
+        #take first 20 characters for the ip header
+        ip_header = packet[eth_length:20+eth_length]
 
-		#now unpack them :)
-		iph = unpack('!BBHHHBBH4s4s' , ip_header)
+        #now unpack them :)
+        iph = unpack('!BBHHHBBH4s4s' , ip_header)
 
-		version_ihl = iph[0]
-		version = version_ihl >> 4
-		ihl = version_ihl & 0xF
+        version_ihl = iph[0]
+        version = version_ihl >> 4
+        ihl = version_ihl & 0xF
 
-		iph_length = ihl * 4
+        iph_length = ihl * 4
 
-		ttl = iph[5]
-		protocol = iph[6]
-		s_addr = socket.inet_ntoa(iph[8]);
-		d_addr = socket.inet_ntoa(iph[9]);
+        ttl = iph[5]
+        protocol = iph[6]
+        s_addr = socket.inet_ntoa(iph[8])
+        d_addr = socket.inet_ntoa(iph[9])
 
-#		print 'Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr)
-		return s_addr, d_addr
+        return s_addr, d_addr
 
 
 
